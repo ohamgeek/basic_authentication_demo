a438bdf7e8f7bb4b71e3f70efd8c1b989044f85c
Meghan Hoke <ohamgeek@gmail.com>
2013-05-04 12:27:58 -0400

    Working authentication demo from class 28

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authentication::Application.config.secret_token = '994596cd6caf6b5b6e8d6f20b295b1bbc1dab1bf9204ae651df298f8d6c1c95baa7a15f576d019f9676d2f792b403932067cd9ea3094b671d0169c7aa85f80d0'
